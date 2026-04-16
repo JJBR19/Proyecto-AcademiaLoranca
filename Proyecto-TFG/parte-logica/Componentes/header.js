319ea41fac609cc8a347565da4e0b90ccd7c1363
Julio <jjbr19@educa.madrid.org>
2026-04-16 12:40:03 +0200

    Commit titulo form, header, whatsapp y grupos

--- Proyecto-TFG/parte-logica/Componentes/header.js
+++ Proyecto-TFG/parte-logica/Componentes/header.js
@@ -144,7 +144,6 @@ class HeaderComponent extends HTMLElement {
                 opacity:0;
                 transform: translateY(-10px);
                 pointer-events:none;
-                transition: all .2s ease;
 
                 margin:0;
             }
